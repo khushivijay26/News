cffc46177913594f16cd22f8506a6b0c31018d6a
Khushi Vijay <khushivijay176@gmail.com>
2023-03-03 23:00:48 +0530

    favicon added

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,7 +5,7 @@ const NewsItem = ({ title, description, url, urlToImage }) => {
     return (
         <div className='news-app'>
             <div className='news-item'>
-                <img className='news-img' src={urlToImage} alt={urlToImage} />
+                <img className='news-img' src={urlToImage} alt="Unable to load" />
                 <h3><a href={url}>{title}</a></h3>
                 <p>{description}</p>
             </div>
